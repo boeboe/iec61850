c759c32b9fd333d62b0bff354cb288a3db42e18f
Bart Van Bos <bart@tetrate.io>
2026-02-16 11:05:32 +0100

    try goose on all platform/arch combos

--- config_win64.go
+++ config_win64.go
@@ -3,5 +3,5 @@
 package iec61850
 
 // #cgo CFLAGS: -Wno-builtin-declaration-mismatch -I./libiec61850/inc/hal/inc -I./libiec61850/inc/common/inc -I./libiec61850/inc/goose -I./libiec61850/inc/sampled_values -I./libiec61850/inc/iec61850/inc -I./libiec61850/inc/logging -I./libiec61850/inc/mms -I./libiec61850/inc/r_session
-// #cgo LDFLAGS: -static -static-libgcc -static-libstdc++ -L${SRCDIR}/libiec61850/lib/win64 -Wl,--whole-archive ${SRCDIR}/libiec61850/lib/win64/libiec61850.a ${SRCDIR}/libiec61850/lib/win64/libhal.a -Wl,--no-whole-archive -lmbedtls -lmbedx509 -lmbedcrypto -lwpcap -lpacket -lws2_32 -liphlpapi -lbcrypt -lmingwex
+// #cgo LDFLAGS: -static -static-libgcc -static-libstdc++ -L${SRCDIR}/libiec61850/lib/win64 ${SRCDIR}/libiec61850/lib/win64/libiec61850.a ${SRCDIR}/libiec61850/lib/win64/libhal.a -lmbedtls -lmbedx509 -lmbedcrypto -lwpcap -lpacket -lws2_32 -liphlpapi -lbcrypt -lmingwex
 import "C"
